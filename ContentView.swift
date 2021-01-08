551100999db545f7427bfab8d88ed62c5c0fadd0
David Razmadze <75696759+davidrazmadzeExtra@users.noreply.github.com>
2021-01-08 11:03:38 -0500

    Add files via upload

---
+++ ContentView.swift
@@ -0,0 +1,53 @@
+//
+//  ContentView.swift
+//  Admob_SwiftUI_DEMO
+//
+//  Created by davidraz on 1/8/21.
+//
+
+import SwiftUI
+import GoogleMobileAds
+
+struct ContentView: View {
+    var body: some View {
+      VStack {
+        Text("Google Admob")
+        
+        GADBannerViewController()
+      }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+// MARK: - GADBannerViewController
+
+struct GADBannerViewController: UIViewControllerRepresentable {
+  
+  func makeUIViewController(context: Context) -> some UIViewController {
+    let view = GADBannerView(adSize: kGADAdSizeBanner)
+    let viewController = UIViewController()
+    let testID = "ca-app-pub-3940256099942544/2934735716"
+    let realID = "ca-app-pub-9450475706334809/7473547174"
+    
+    // Banner Ad
+    view.adUnitID = realID
+    view.rootViewController = viewController
+    
+    // View Controller
+    viewController.view.addSubview(view)
+    viewController.view.frame = CGRect(origin: .zero, size: kGADAdSizeBanner.size)
+    
+    // Load an ad
+    view.load(GADRequest())
+    
+    return viewController
+  }
+  
+  func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {}
+  
+}
